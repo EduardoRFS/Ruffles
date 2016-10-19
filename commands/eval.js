58b0947452447cc95cdd38d92bb43abcb8fbb48d
EduardoRFS <theeduardorfs@gmail.com>
2016-10-19 02:19:40 -0200

    eval

--- commands/eval.js
+++ commands/eval.js
@@ -7,6 +7,10 @@ exports.onlyOwner = true;
 
 async function onEvalCommand (group, message, ...args) {
   if (message.userName !== config.owner) return group.message('Haha, até a rayssa é mais inteligente que você');
-  let command = args.join(' ').replace(/\r/g, '\n');
-  eval(command);
+  try {
+    let command = args.join(' ').replace(/\r/g, '\n');
+    eval(command);
+  } catch (err) {
+    group.message(err.stack.toString().slice(0, 800));
+  }
 }
