bae7694ec9cbe25206c884ed34e38d4f85596fa9
EduardoRFS <theeduardorfs@gmail.com>
2016-10-19 02:24:35 -0200

    eval

--- commands/eval.js
+++ commands/eval.js
@@ -5,11 +5,13 @@ module.exports = exports = onEvalCommand;
 exports.names = ['eval', 'exec'];
 exports.onlyOwner = true;
 
+const base = '(async function () { fnbody }).apply(this, arguments)';
+
 async function onEvalCommand (group, message, ...args) {
   if (message.userName !== config.owner) return group.message('Haha, até a rayssa é mais inteligente que você');
   try {
     let command = args.join(' ').replace(/\r/g, '\n');
-    eval(command);
+    eval(base.replace('fncode', command));
   } catch (err) {
     group.message(err.stack.toString().slice(0, 800));
   }
